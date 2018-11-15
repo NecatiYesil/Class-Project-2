f7f42c10661b2c0cd70327ae807e8b79683642d3
Joshua Santillan <santj96@gmail.com>
2018-11-15 12:35:19 -0800

    deck.h

--- deck.h
+++ deck.h
@@ -1,1 +1,15 @@
+#ifndef DECK_H
+#define DECK_H
 
+class Deck
+{
+    private:
+        
+
+    public:
+        void shuffle(std::vector<Card>); // vector holding 52 cardsrandomize card elements using vector
+        void deal (int n); // returns top (n) cards of deck, return vector of cards; internal for in deck cards.
+
+
+};
+#endif
